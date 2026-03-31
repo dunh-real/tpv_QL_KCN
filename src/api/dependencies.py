65c0bd6b5d2d813fb34044170170a5d72165012d
DoanThaiA <doanthai288@gmail.com>
2026-03-31 11:23:54 +0700

    llm vectordb cached

--- src/api/dependencies.py
+++
@@ -1,6 +0,0 @@
-from src.core.config import settings
-from src.db.hyperspace import EnterpriseDocumentStore
-
-def get_vector_db():
-    return EnterpriseDocumentStore(settings.HYPERSPACE_HOST, settings.HYPERSPACE_API_KEY, settings.HYPERSPACE_COLLECTION_NAME
-    )
